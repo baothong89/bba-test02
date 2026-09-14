9281e165f82c1239336f942edf9a724c6b869a7c
test 02 - BaoThong <baothong8912@gmail.com>
2026-09-14 12:41:02 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/calm-belt-2.js
@@ -0,0 +1,18 @@
+let devilFruit = "gomu-gomu";
+let effect = "";
+if (devilFruit === "gomu-gomu" ){
+    effect = "Gear 5 unlocked!";
+    console.log(effect);
+}else if (devilFruit === "gomu-gomu") {
+    effect = "Flame Emperor!";
+    console.log(effect);
+}else if (devilFruit === "ope-ope"){
+    effect = "Room and Ope!";
+    console.log(effect);
+}else if(devilFruit === "none"){
+    effect = "Normal Human";
+    console.log(effect);
+}else{
+    effect = "Unknow devil fruit";
+    console.log(effect);
+}
