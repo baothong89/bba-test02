cd04520fe8e81c816ef7fda9916c83b8c813582e
test 02 - BaoThong <baothong8912@gmail.com>
2026-09-14 12:31:02 +0700

    feat: Add solution for calm belt challenge

---
+++ tests/lesson-04/calm-belt.js
@@ -0,0 +1,10 @@
+let navigatorName = "Luffy";
+let currentHakiLevel = 3;
+const islandA = 15;
+const islandB = 25;
+const islandC = 40;
+let totalMeatConsume = islandA + islandB + islandC;
+let averageMeatConsume = totalMeatConsume/3;
+console.log(`Lượng thịt còn dư là: ${totalMeatConsume - averageMeatConsume}`)
+
+
