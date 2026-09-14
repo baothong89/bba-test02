cb57359c5b380eb5e660c9190f60731d71d8215b
test 02 - BaoThong <baothong8912@gmail.com>
2026-09-14 21:36:30 +0700

    feat: add printBountyLeaderboard

---
+++ tests/lesson-04/the-final-stage.js
@@ -0,0 +1,15 @@
+// tạo mảng 1-100
+const poneglyph=[];
+for(let i = 1; i <= 10; i++){
+    poneglyph.push(i);
+};
+let laughTale = 0;
+for(let i = 1; i <= poneglyph.length; i++){
+    for (let j = i; j <= poneglyph.length; j++){
+        if((i*j)%19 === 0){
+            console.log(`Cặp (${i},${j}) là 1 cặp Laugh Tale `);
+            laughTale++;
+        };
+    };
+};
+console.log(`Tổng số cặp  Laugh Tale là: ${laughTale} `);
