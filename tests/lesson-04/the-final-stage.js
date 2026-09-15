6008b98e07650346a147a013b035d6f045f7af39
test 02 - BaoThong <baothong8912@gmail.com>
2026-09-15 16:21:07 +0700

    fix: add 2nd way for final stage

--- tests/lesson-04/the-final-stage.js
+++ tests/lesson-04/the-final-stage.js
@@ -6,10 +6,24 @@ for(let i = 1; i <= 100; i++){
 let laughTale = 0;
 for(let i = 1; i <= poneglyph.length; i++){
     for (let j = i; j <= poneglyph.length; j++){
-        if((i*j)%19 === 0){
+        if((i * j) % 19 === 0){
             console.log(`Cặp (${i},${j}) là 1 cặp Laugh Tale `);
             laughTale++;
         };
     };
 };
-console.log(`Tổng số cặp  Laugh Tale là: ${laughTale} `);
+console.log(`Tổng số cặp Laugh Tale là: ${laughTale} `);
+
+//Dùng hàm reduce của array
+
+let total = poneglyph.reduce((total,currentNumber) => {
+    for (let i = currentNumber; i <= poneglyph.length; i++){
+        if((currentNumber * i) % 19 === 0){
+            console.log(`Cặp (${currentNumber},${i}) là 1 cặp Laugh Tale `);
+            total++;
+        };
+    };
+    return total;
+},0);
+console.log(`Tổng số cặp Laugh Tale là: ${total} `);
+
